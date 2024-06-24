6a6166db744e8aabc3e8701f0a9061a022bc624c
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-06-24 12:50:25 +0000

    Customisation

--- next-app-template/app/page.tsx
+++ next-app-template/app/page.tsx
@@ -10,9 +10,9 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+      <div className="inline-block max-w-lg text-center items-left justify-center">
         <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <h1 className={title({ color: "blue" })}>beautiful&nbsp;</h1>
         <br />
         <h1 className={title()}>
           websites regardless of your design experience.
@@ -20,28 +20,6 @@ export default function Home() {
         <h2 className={subtitle({ class: "mt-4" })}>
           Beautiful, fast and modern React UI library.
         </h2>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
       </div>
 
       <div className="mt-8">
--- next-app-template/config/site.ts
+++ next-app-template/config/site.ts
@@ -9,20 +9,16 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
       label: "Blog",
       href: "/blog",
     },
     {
-      label: "About",
-      href: "/about",
+      label: "About me",
+      href: "/about,
+    },
+    {
+      label: "Projects",
+      href: "/projects",
     },
   ],
   navMenuItems: [
@@ -61,9 +57,5 @@ export const siteConfig = {
   ],
   links: {
     github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
   },
 };
