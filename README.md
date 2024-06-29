7cd1ef30733a7d23781c35e538add7feb9221df9
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-06-29 19:20:06 +0000

    Added the plane

---
+++ README.md
@@ -0,0 +1,53 @@
+# The Plan...
+
+Make a very simple website, the code can suck, it already does: it's in typescript
+
+
+## MOSCOWS
+
+### Must have...
+
+- Mobile compatibility
+- Contact page
+- Projects list
+- Bio
+- Dark mode
+- Ability to add new projects easily
+
+### Should have
+
+- Search function
+- Projects ordered by date
+- Nice url
+- Images in projects list
+
+### Could have
+
+- Light and dark mode
+- Nice animations
+
+### Won't have
+
+- More than 4 main pages
+
+## Structure
+
+- Navbar
+    - Compatible with small screens
+
+- Home page 
+    - contains bio
+
+- Blog/Projects page 
+    - contains any projects in a list ordered by date with a search function
+
+- Contact page
+    - Contains email address/contact box
+
+## Limitations and strategy
+
+- Idrk web dev
+- For theming it is better to have the 'icons' in typescript to allow easy colour manipulation
+- Theming done through tailwind
+- Conditional layout
+- Static images in /public/assets
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,124 +18,67 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-  TwitterIcon,
   GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
   Logo,
 } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NextLink>
-        </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </div>
+      <NextUINavbar
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-primary",
+        ],
+      }}
+    >
+      <NavbarBrand>
+        <Logo />
+        <p className="start font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {siteConfig.navItems.map((item) => (
+          <NavbarItem key={item.href}>
+            <Link color="foreground ">
+              {item.label}
+             </Link>
+          </NavbarItem>))}
       </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter}>
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord}>
-            <DiscordIcon className="text-default-500" />
-          </Link>
+      <NavbarContent className="flex gap-2" justify="end">
+        <NavbarItem>
           <Link isExternal href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
+          <GithubIcon className="text-default-500"/>
           </Link>
-          <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
+        {siteConfig.navItems.map((item) => (
+          <NavbarMenuItem key={item.href}>
+            <Link
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
       </NavbarMenu>
+      <NavbarContent className="sm:hidden" justify="end">
+      <NavbarMenuToggle />
+      </NavbarContent>
     </NextUINavbar>
+
   );
 };
