3483875a2303badd2596c864482577d7ebd881ac
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-07-03 08:18:13 +0100

    Added some stuff

--- app/config/site.ts
+++ app/config/site.ts
@@ -17,25 +17,7 @@ export const siteConfig = {
       href: "/contact",
     },
   ],
-  navMenuItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Contact",
-      href: "/contact",
-    },
-  ],
   links: {
     github: "https://github.com/SemicolonUnexpected",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
   },
 };
