38567b85efa8d67c9df7642970c3f825200878a8
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-07-30 00:36:52 +0000

    Prepare for bundle

--- components/terminal-typing-animation.tsx
+++ components/terminal-typing-animation.tsx
@@ -1,12 +1,49 @@
 "use client";
 
-import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { motion, useMotionValue, useTransform, animate, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import CursorBlinker from "@/components/cursor-blinker";
 
 import clsx from "clsx";
 import { fontMono } from "@/config/fonts";
 
+export default function TypingAnimation() {
+  const text = "whoami";
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    "$ " + text.slice(0, latest)
+  );
+
+  const animation = useAnimation();
+  animation.stop
+
+  useEffect(() => {
+  }, []);
+
+  async function doAnimation() {
+    await animation.start()
+  }
+
+  return (
+    <div>
+      <div>
+        <motion.span className={clsx(
+          "text-4xl text-blue-600 font-sans",
+          fontMono.variable
+        )}>
+          {displayText}
+        </motion.span>
+        <CursorBlinker/>
+      </div>
+
+      <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, repeat: Infinity }}>
+        Hi there
+      </motion.div>
+    </div>
+  );
+}
+/*
 export default function TypingAnimation() {
     const text = "whoami";
     const count = useMotionValue(0);
@@ -42,3 +79,4 @@ export default function TypingAnimation() {
     </div>
     );
 }
+*/
