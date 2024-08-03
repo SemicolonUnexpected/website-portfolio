c264c791fcf60e501a274194ed35e6acb2b4d64f
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-03 07:51:59 +0000

    Started improving project card

--- components/programming-languages.tsx
+++ components/programming-languages.tsx
@@ -2,9 +2,9 @@ import { CSharp, Typescript, Python } from "@/components/icons";
 
 export default function ProgrammingLanguages() {
   return(
-    <div className="flex">
+    <div className="relative flex w-full h-80">
       <CSharp size={90} className=""/>
-      <Typescript size={90} className=""/>
+      <Typescript size={90} className="absolute bottom-0 right-64"/>
       <Python size={90} className=""/>
     </div>
   );
--- components/project-card.tsx
+++ components/project-card.tsx
@@ -6,38 +6,32 @@ import { Image } from "@nextui-org/image";
 
 import * as React from "react";
 
-interface CardProps {
+interface ProjectCardProps {
   title: string;
   text: string;
   route: string;
   imageSrc?: string;
   alt?: string;
-  isIcon?: boolean;
 }
 
-interface ImageProps {
-}
-
-type ProjectCardProps = CardProps | ( CardProps & ImageProps )
-
-const ProjectCard: React.FC<ProjectCardProps> = ({ title, text, route, imageSrc="", alt="", isIcon=false}) => {
+export const ProjectCard: React.FC<ProjectCardProps> = ({ title, text, route, imageSrc="", alt=""}) => {
   const router = useRouter();
 
   return (
     <Card className="max-w-5xl h-96 w-full md:h-48" isPressable onPress={() => router.push(route)}>
       <CardBody>
         <div className="flex flex-col md:flex-row gap-4">
-            {imageSrc && !isIcon &&
+            {imageSrc &&
             <div className="relative basis-1/2">
               <Image src={imageSrc} alt={alt} width="100% md:auto" className="object-cover basis-1/2 max-h-48 md:max-h-[10.5rem]"/>
             </div>
             }
-          <div className={(imageSrc && !isIcon) ? "basis-1/2" : ""}>
+          <div className={imageSrc &&"basis-1/2"}>
             <h1 className="font-semibold text-xl">{title}</h1>
             <p>{text}</p>
           </div>
 
-          {imageSrc && isIcon &&
+          {imageSrc &&
             <div>
               <Image src={imageSrc} alt={alt} width="64" height="64" className="mt-auto md:ml-auto"/>
             </div>
@@ -48,5 +42,36 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ title, text, route, imageSrc=
   );
 };
 
-export default ProjectCard;
+interface IconProjectCardProps extends ProjectCardProps {
+  imageSrc?: never;
+  icon: string;
+}
 
+
+export const IconProjectCard: React.FC<IconProjectCardProps> = ({ title, text, route, imageSrc="", alt=""}) => {
+  const router = useRouter();
+
+  return (
+    <Card className="max-w-5xl h-96 w-full md:h-48" isPressable onPress={() => router.push(route)}>
+      <CardBody>
+        <div className="flex flex-col md:flex-row gap-4">
+            {imageSrc &&
+            <div className="relative basis-1/2">
+              <Image src={imageSrc} alt={alt} width="100% md:auto" className="object-cover basis-1/2 max-h-48 md:max-h-[10.5rem]"/>
+            </div>
+            }
+          <div className={imageSrc &&"basis-1/2"}>
+            <h1 className="font-semibold text-xl">{title}</h1>
+            <p>{text}</p>
+          </div>
+
+          {imageSrc &&
+            <div>
+              <Image src={imageSrc} alt={alt} width="64" height="64" className="mt-auto md:ml-auto"/>
+            </div>
+          }
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
