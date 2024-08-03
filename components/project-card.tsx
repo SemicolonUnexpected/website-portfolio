84fc609baa4d34db2f47208fa4bb48722a2fade7
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-03 08:05:40 +0000

    Not working will come back to later

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -3,6 +3,7 @@
 import { Card, CardBody } from "@nextui-org/card";
 import { useRouter } from "next/navigation";
 import { Image } from "@nextui-org/image";
+import { IconSvgProps } from "@/types";
 
 import * as React from "react";
 
@@ -30,46 +31,36 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ title, text, route, im
             <h1 className="font-semibold text-xl">{title}</h1>
             <p>{text}</p>
           </div>
-
-          {imageSrc &&
-            <div>
-              <Image src={imageSrc} alt={alt} width="64" height="64" className="mt-auto md:ml-auto"/>
-            </div>
-          }
         </div>
       </CardBody>
     </Card>
   );
 };
 
-interface IconProjectCardProps extends ProjectCardProps {
-  imageSrc?: never;
-  icon: string;
+interface IconProjectCardProps {
+  title: string;
+  text: string;
+  route: string;
+  IconComponent?: React.ComponentType<IconSvgProps>;
 }
 
 
-export const IconProjectCard: React.FC<IconProjectCardProps> = ({ title, text, route, imageSrc="", alt=""}) => {
+export const IconProjectCard: React.FC<IconProjectCardProps> = ({ title, text, route, IconComponent }) => {
   const router = useRouter();
 
   return (
     <Card className="max-w-5xl h-96 w-full md:h-48" isPressable onPress={() => router.push(route)}>
       <CardBody>
         <div className="flex flex-col md:flex-row gap-4">
-            {imageSrc &&
-            <div className="relative basis-1/2">
-              <Image src={imageSrc} alt={alt} width="100% md:auto" className="object-cover basis-1/2 max-h-48 md:max-h-[10.5rem]"/>
-            </div>
-            }
-          <div className={imageSrc &&"basis-1/2"}>
+          <div className="w-full">
             <h1 className="font-semibold text-xl">{title}</h1>
             <p>{text}</p>
           </div>
 
-          {imageSrc &&
-            <div>
-              <Image src={imageSrc} alt={alt} width="64" height="64" className="mt-auto md:ml-auto"/>
-            </div>
-          }
+          <div>
+            <IconComponent/>
+          </div>
+
         </div>
       </CardBody>
     </Card>
