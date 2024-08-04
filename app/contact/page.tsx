ca1d4bdfc7383a297c4e2a3e2a7813fd9d7e9901
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-04 12:33:39 +0000

    Started on nodemailer

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,23 +2,40 @@ import { title } from "@/components/primitives";
 import { Input, Textarea } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 
-import clsx from "clsx";
+import nodemailer from "nodemailer";
 
 export default function ContactPage() {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "semicolonunexpected@gmail.com",
+      pass: "",
+    },
+  })
+
   return (
     <div className="space-y-4">
-        <h1 className={title()}>Got questions?</h1>
-        <div className="text-center">
-                Please feel free to contact me with any queries, questions or thoughts on my projects using the form below
-        </div>
+      <h1 className={title()}>Got questions?</h1>
+      <div className="text-center">
+        Please feel free to contact me with any queries, questions or thoughts on my projects using the form below
+      </div>
 
-        <Input label="Name"/>
+      <Input label="Name"/>
 
-        <Input type="email" label="Email"/>
+      <Input type="email" label="Email"/>
 
-        <Textarea label="Your message" minRows={8}/>
+      <Textarea label="Your message" minRows={8}/>
 
-        <Button color="primary" size="lg">Submit</Button>
+      <Button color="primary" size="lg" onPress={() => {
+        transporter.sendMail({
+          from: {}
+        })
+      }
+      }>
+        Submit
+      </Button>
 
     </div>
   );
