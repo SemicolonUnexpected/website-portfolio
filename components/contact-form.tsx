4b4e2b084cc25bb978426a62a8ba54a7027b0b7e
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-08 11:58:11 +0000

    got error messages working, gotta fix the button positioning

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -15,19 +15,20 @@ export default function ContactForm() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const [nameInvalid, setNameInvalid] = useState(true);
+  const [nameInvalid, setNameInvalid] = useState(false);
   const [emailInvalid, setEmailInvalid] = useState(false);
   const [messageInvalid, setMessageInvalid] = useState(false);
 
   const [loading, setLoading] = useState(false);
 
   return(
-    <>
+    <div className="text-start space-y-4">
       <Input
         label="Name"
         value={name}
         onValueChange={setName}
         isInvalid={nameInvalid}
+        errorMessage="Please enter a valid name"
         isRequired
       />
 
@@ -36,6 +37,8 @@ export default function ContactForm() {
         label="Email"
         value={email}
         onValueChange={setEmail}
+        isInvalid={emailInvalid}
+        errorMessage="Please enter a valid email"
         isRequired
       />
 
@@ -44,23 +47,40 @@ export default function ContactForm() {
         minRows={8}
         value={message}
         onValueChange={setMessage}
+        isInvalid={messageInvalid}
+        errorMessage="Please enter a valid message"
         isRequired
       />
 
       <Button
+        className="mx-auto justify-center self-center"
         color="primary"
         size="lg"
         type="submit"
         isLoading={loading}
         onPress={async () => {
           // Validation
-          if (name === "") {}
-          if (!validateEmail(email)) {}
-          if(message === "") {
-
+          let error = false;
+          if (name === "") {
+            setNameInvalid(true);
+            error = true;
           }
+          else setNameInvalid(false);
+          if (!validateEmail(email)) {
+            setEmailInvalid(true);
+            error = true;
+          }
+          else setEmailInvalid(false);
+          if(message === "") {
+            setMessageInvalid(true);
+            error = true;
+          }
+          else setMessageInvalid(false);
+
+          if (error) return;
 
           setLoading(true);
+
           // Send message
           await sendEmail(name, email, message);
 
@@ -74,9 +94,6 @@ export default function ContactForm() {
       >
         Submit
       </Button>
-      <div>
-        Oops, there was an error while trying to send your email
-      </div>
-    </>
+    </div>
   );
 }
